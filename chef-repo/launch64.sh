3c0e64b3139cd4392227821c95088e534f0b73b0
James Wickett <wickett@gmail.com>
2013-09-18 16:18:20 -0500

    moving to new structure

---
+++ chef-repo/launch64.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+echo "Launching ec2 instance..."
+cd ./chef-repo
+knife ec2 server create -r "recipe[cloud-dev]" -I ami-7d317314 -G default -S gary -N raring-dev-box.1 -f t1.micro
+cd ..
--- launch64.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-echo "Launching ec2 instance..."
-cd ./chef-repo
-knife ec2 server create -r "role[gary]" -I ami-7d317314 -G default -S gary -N raring-dev-box.1 -f t1.micro
-cd ..
