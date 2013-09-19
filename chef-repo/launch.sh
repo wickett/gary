721aa367cc3cff0c980370c2afdbb1fda035bfb1
James Wickett <wickett@gmail.com>
2013-09-19 13:58:18 -0500

    updated launch and cookbooks

--- chef-repo/launch.sh
+++ chef-repo/launch.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+if [ -z "$1" ]; then
+  echo "Usage: ./launch.sh [node_name]"
+  exit
+fi
+  
+echo "Updating cookbooks"
+librarian-chef update
+
+echo "Uploading cookbooks"
+knife cookbook upload -a
 
 echo "Launching ec2 instance..."
-knife ec2 server create -r "recipe[cloud-dev]" -I ami-7d317314 -G default -S gary -N raring-dev-box.10 -f t1.micro
-cd ..
+knife ec2 server create -r "recipe[cloud-dev]" -I ami-7d317314 -G default -S gary -N $1 -f t1.micro
